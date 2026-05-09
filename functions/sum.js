62a548333d13117c3722a359aefefb18e6c63033
kumarmudur <kumarmudur10@gmail.com>
2026-05-09 15:38:16 +0530

    sum example

---
+++ functions/sum.js
@@ -0,0 +1,10 @@
+// sum of two integers
+
+//Write a Program that defines a function to calculate the sum of two integers and
+// prints the result. Call this function by passing two integer values.
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(1, 2));
