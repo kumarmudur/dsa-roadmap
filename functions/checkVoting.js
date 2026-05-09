3be3a4da295c02e3993613f4a3250a0e5263026e
kumarmudur <kumarmudur10@gmail.com>
2026-05-09 15:41:45 +0530

    check voting eligibility

---
+++ functions/checkVoting.js
@@ -0,0 +1,9 @@
+// check voting eligibility
+
+function checkVoting(age) {
+    if (age >= 18) {
+        console.log("You are eligible to vote.");
+    } else {
+        console.log("You are not eligible to vote.");
+    }
+}
