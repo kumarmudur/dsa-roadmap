17bc60f8149473c519b11077287d3403faa9ebd6
kumarmudur <kumarmudur10@gmail.com>
2026-05-16 21:53:33 +0530

    check even or odd number example

---
+++ functions/evenOrOdd.js
@@ -0,0 +1,10 @@
+function evenOrOdd(num) {
+    if (num % 2 === 0) {
+        console.log("Even Number");
+    } else {
+        console.log("Odd Number");
+    }
+}
+
+evenOrOdd(18);
+evenOrOdd(5);
